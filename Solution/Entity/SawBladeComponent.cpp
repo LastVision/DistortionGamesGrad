43bcb1762d0a542570a60bb9327b0a3f17742fe7
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-05-03 10:28:20 +0200

    Added patrol to saw blade, unfinished

--- Solution/Entity/SawBladeComponent.cpp
+++ Solution/Entity/SawBladeComponent.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 #include "SawBladeComponent.h"
 
-
-SawBladeComponent::SawBladeComponent()
+SawBladeComponent::SawBladeComponent(Entity& anEntity)
+	: Component(anEntity)
+	, myIsPatrolling(false)
+	, myPositions(8)
 {
 }
-
 
 SawBladeComponent::~SawBladeComponent()
 {
