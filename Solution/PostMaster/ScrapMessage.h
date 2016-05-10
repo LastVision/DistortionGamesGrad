91d8bdd6bae8c78cb93370047a5ac06663be864c
fabs <fabian.ahlund.sp14@thegameassembly.com>
2016-05-10 12:31:36 +0200

    new message

---
+++ Solution/PostMaster/ScrapMessage.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Message.h"
+#include <Vector3.h>
+#include "GameEnum.h"
+
+struct ScrapMessage : public Message
+{
+	ScrapMessage(eScrapPart aPart, const CU::Vector3<float>& aPosition, const CU::Vector3<float>& aVelocity);
+	
+
+	const eScrapPart myScrapPart;
+	const CU::Vector3<float> myPosition;
+	const CU::Vector3<float> myVelocity;
+};
+
+inline ScrapMessage::ScrapMessage(eScrapPart aPart, const CU::Vector3<float>& aPosition, const CU::Vector3<float>& aVelocity)
+	: Message(eMessageType::SCRAP)
+	, myScrapPart(aPart)
+	, myPosition(aPosition)
+	, myVelocity(aVelocity)
+{
+}
+
+
