b0d6a01fa2e635a70f6ed292b995cdb49294ff2f
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-05-11 15:31:02 +0200

    Fixed steam

--- Solution/Entity/Movement.cpp
+++ Solution/Entity/Movement.cpp
@@ -7,6 +7,7 @@ Movement::Movement(const MovementComponentData& aData, CU::Matrix44f& anOrientat
 	, myOrientation(anOrientation)
 	, myMovementComponent(aMovementComponent)
 	, myIsActive(false)
+	, myIsInSteam(false)
 {
 	myRaycastHandler = [=](PhysicsComponent* aComponent, const CU::Vector3<float>& aDirection, const CU::Vector3<float>& aHitPosition, const CU::Vector3<float>& aHitNormal)
 	{
