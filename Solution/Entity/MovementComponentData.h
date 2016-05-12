7f985a97226bb060553309eb935beb36d447bfbe
nian0601 <anderssonniklas@outlook.com>
2016-05-12 13:58:28 +0200

    Default initialized all ComponentDatas's

--- Solution/Entity/MovementComponentData.h
+++ Solution/Entity/MovementComponentData.h
@@ -3,18 +3,18 @@
 struct MovementComponentData
 {
 	bool myExistsInEntity = false;
-	float myGravity;
-	float myImpulse;
-	float myRotationSpeed;
-	float myDeadZone;
-	float myAngleEpsilon;
-	float myWalkSpeed;
-	float myWalkDrag;
-	float myDashAimTime;
-	float myDashFlyTime;
-	float myDashDistance;
-	float myDashCooldown;
-	float myDashSpeed;
-	float myMaxAngleWhenLanding;
+	float myGravity = 0.f;
+	float myImpulse = 0.f;
+	float myRotationSpeed = 0.f;
+	float myDeadZone = 0.f;
+	float myAngleEpsilon = 0.f;
+	float myWalkSpeed = 0.f;
+	float myWalkDrag = 0.f;
+	float myDashAimTime = 0.f;
+	float myDashFlyTime = 0.f;
+	float myDashDistance = 0.f;
+	float myDashCooldown = 0.f;
+	float myDashSpeed = 0.f;
+	float myMaxAngleWhenLanding = 0.f;
 	CU::Vector2<float> myDrag;
 };
