569744857773fb04a620d1efc3c558ab2bee8e14
nian0601 <anderssonniklas@outlook.com>
2016-05-23 16:10:27 +0200

    Cleaned up Renderer

--- Solution/Engine/Renderer.h
+++ Solution/Engine/Renderer.h
@@ -23,42 +23,19 @@ namespace Prism
 		~Renderer();
 
 		void ProcessShadow(SpotLightShadow* aShadowSpotLight, Scene* aScene);
-
-		void BeginScene();
-		void EndScene(int aEffect);
-
-		void FinalRender();
-
 		void Render(Texture* aSource, Texture* aEmissiveTexture, Texture* aDepthStencilTexture, int aEffect);
 
-		void OnResize(float aWidth, float aHeight);
-
-		Texture* GetWorldTexture();
 		void SetRenderTargets(ID3D11RenderTargetView* aRenderTarget, ID3D11DepthStencilView* aDepthBuffer);
 
 	private:
-		struct SceneData
-		{
-			Texture* myScene;
-			Texture* myFinished;
-		};
 		
-		Engine* myEngine;
-		SceneData mySceneData[MAX_NUMBER_OF_SCENES];
-
 		FullScreenHelper* myFullScreenHelper;
 		float myClearColor[4];
-		int mySceneIndex;
+		
 
 		Texture* myFinalTexture;
-		Texture* myCombineMiddleMan;
 
 		D3D11_VIEWPORT* myShadowViewport;
 	};
-
-	inline Texture* Renderer::GetWorldTexture()
-	{
-		return mySceneData[1].myScene;
-	}
 }
 
