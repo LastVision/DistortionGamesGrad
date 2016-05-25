909b38578875db4d724b334b55e83bf40f3c89b8
LastVision <daniel.carlsson92@gmail.com>
2016-05-25 11:09:52 +0200

    Added Empty OptionState

---
+++ Solution/Game/OptionState.cpp
@@ -0,0 +1,91 @@
+#include "stdafx.h"
+
+#include <Cursor.h>
+#include <GUIManager.h>
+#include <InputWrapper.h>
+#include <OnClickMessage.h>
+#include "OptionState.h"
+#include <PostMaster.h>
+
+
+OptionState::OptionState()
+{
+}
+
+
+OptionState::~OptionState()
+{
+	SAFE_DELETE(myGUIManager);
+	myStateStack = nullptr;
+	myCursor = nullptr;
+	myController = nullptr;
+	PostMaster::GetInstance()->UnSubscribe(this, 0);
+}
+
+void OptionState::InitState(StateStackProxy* aStateStackProxy, CU::ControllerInput* aController, GUI::Cursor* aCursor)
+{
+	myStateStack = aStateStackProxy;
+	myController = aController;
+	myCursor = aCursor;
+	myStateStatus = eStateStatus::eKeepState;
+	myIsActiveState = true;
+	myGUIManager = new GUI::GUIManager(myCursor, "Data/Resource/GUI/GUI_level_select.xml", nullptr, -1);
+	myCursor->SetShouldRender(true);
+	InitControllerInMenu(myController, myGUIManager);
+	PostMaster::GetInstance()->Subscribe(this, eMessageType::ON_CLICK);
+}
+
+void OptionState::EndState()
+{
+	myIsActiveState = false;
+	myCursor->SetShouldRender(false);
+}
+
+const eStateStatus OptionState::Update(const float& aDeltaTime)
+{
+	if (CU::InputWrapper::GetInstance()->KeyDown(DIK_ESCAPE) == true)
+	{
+		myIsActiveState = false;
+		myCursor->SetShouldRender(false);
+		return eStateStatus::ePopMainState;
+	}
+
+	HandleControllerInMenu(myController, myGUIManager);
+
+	myGUIManager->Update(aDeltaTime);
+
+	return myStateStatus;
+}
+
+void OptionState::Render()
+{
+	myGUIManager->Render();
+}
+
+void OptionState::ResumeState()
+{
+	myIsActiveState = true;
+	myCursor->SetShouldRender(true);
+	InitControllerInMenu(myController, myGUIManager);
+	PostMaster::GetInstance()->Subscribe(this, eMessageType::ON_CLICK);
+}
+
+void OptionState::PauseState()
+{
+	PostMaster::GetInstance()->UnSubscribe(this, eMessageType::ON_CLICK);
+}
+
+void OptionState::OnResize(int aWidth, int aHeight)
+{
+	myGUIManager->OnResize(aWidth, aHeight);
+}
+
+void OptionState::ReceiveMessage(const OnClickMessage& aMessage)
+{
+	switch (aMessage.myEvent)
+	{
+	case eOnClickEvent::GAME_QUIT:
+		myStateStatus = eStateStatus::ePopMainState;
+		break;
+	}
+}
