ad2ed05acf3b74d08917e99e14aa65ce1d470aef
fabs <fabian.ahlund.sp14@thegameassembly.com>
2016-05-31 11:13:46 +0200

    cleaned state

--- Solution/Game/HatUnlockState.cpp
+++ Solution/Game/HatUnlockState.cpp
@@ -1,12 +1,95 @@
 #include "stdafx.h"
+#include <ControllerInput.h>
+#include <Cursor.h>
 #include "HatUnlockState.h"
-
+#include <HatManager.h>
+#include <GUIManager.h>
+#include <InputWrapper.h>
+#include <ModelLoader.h>
+#include <OnClickMessage.h>
+#include <PostMaster.h>
+#include <SpriteProxy.h>
 
 HatUnlockState::HatUnlockState()
+	: myGUIManager(nullptr)
 {
 }
 
-
 HatUnlockState::~HatUnlockState()
 {
+	PostMaster::GetInstance()->UnSubscribe(this, 0);
+	SAFE_DELETE(myGUIManager);
+	myCursor = nullptr;
+}
+
+void HatUnlockState::InitState(StateStackProxy* aStateStackProxy, CU::ControllerInput* aController, GUI::Cursor* aCursor)
+{
+	myIsActiveState = true;
+	myIsLetThrough = false;
+	myStateStatus = eStateStatus::eKeepState;
+	myStateStack = aStateStackProxy;
+	myCursor = aCursor;
+	myController = aController;
+	myCursor->SetShouldRender(true);
+	myGUIManager = new GUI::GUIManager(myCursor, "Data/Resource/GUI/GUI_hat_unlock.xml", nullptr, -1);
+
+	CU::Vector2<int> windowSize = Prism::Engine::GetInstance()->GetWindowSizeInt();
+	OnResize(windowSize.x, windowSize.y);
+
+	InitControllerInMenu(myController, myGUIManager, myCursor);
+	myController->SetIsInMenu(true);
+
+	PostMaster::GetInstance()->Subscribe(this, eMessageType::ON_CLICK);
+}
+
+void HatUnlockState::EndState()
+{
+}
+
+void HatUnlockState::OnResize(int aWidth, int aHeight)
+{
+	myGUIManager->OnResize(aWidth, aHeight);
+}
+
+const eStateStatus HatUnlockState::Update(const float& aDeltaTime)
+{
+	if (CU::InputWrapper::GetInstance()->KeyDown(DIK_ESCAPE) == true || myController->ButtonOnDown(eXboxButton::BACK)
+		|| myController->ButtonOnDown(eXboxButton::B))
+	{
+		myIsActiveState = false;
+		myCursor->SetShouldRender(false);
+		return eStateStatus::ePopSubState;
+	}
+
+	HandleControllerInMenu(myController, myGUIManager);
+	myGUIManager->Update(aDeltaTime);
+
+
+	return myStateStatus;
+}
+
+void HatUnlockState::Render()
+{
+	myGUIManager->Render();
+}
+
+void HatUnlockState::ResumeState()
+{
+	InitControllerInMenu(myController, myGUIManager, myCursor);
+	myController->SetIsInMenu(true);
+}
+
+void HatUnlockState::PauseState()
+{
+
+}
+
+void HatUnlockState::ReceiveMessage(const OnClickMessage& aMessage)
+{
+	switch (aMessage.myEvent)
+	{
+	case eOnClickEvent::GAME_QUIT:
+		myStateStatus = eStateStatus::ePopSubState;
+		break;
+	}
 }
--- Solution/Game/HatUnlockState.h
+++ Solution/Game/HatUnlockState.h
@@ -1,8 +1,35 @@
 #pragma once
-class HatUnlockState
+#include "Subscriber.h"
+#include "GameState.h"
+
+namespace GUI
+{
+	class GUIManager;
+}
+
+class HatUnlockState : public GameState, public Subscriber
 {
 public:
 	HatUnlockState();
 	~HatUnlockState();
+
+	void InitState(StateStackProxy* aStateStackProxy, CU::ControllerInput* aController, GUI::Cursor* aCursor) override;
+	void EndState() override;
+
+	void OnResize(int aWidth, int aHeight) override;
+
+	const eStateStatus Update(const float& aDeltaTime) override;
+	void Render() override;
+	void ResumeState() override;
+	void PauseState() override;
+
+	void ReceiveMessage(const OnClickMessage& aMessage) override;
+
+
+private:
+	GUI::GUIManager* myGUIManager;
+
+
+
 };
 
