e2d859e56c798689746816c5bab0ceb2f3469736
LastVision <daniel.carlsson92@gmail.com>
2016-06-09 14:23:50 +0200

    Added SQL code for HatStat

--- Solution/CommonUtilities/SQLWrapper.h
+++ Solution/CommonUtilities/SQLWrapper.h
@@ -30,6 +30,7 @@ namespace CU
 		MYSQL_RES* ExecuteQuery(const char* aQuery);
 		void WriteHighscore(const std::string& aUsername, float aTime, int aLevelID);
 		void WriteDeaths(const int aLevelID, const int aNumberOfDeaths);
+		void WriteHatStat(const int aHatID);
 
 		CU::GrowingArray<Highscore> RetriveOnlineHighcore(const int aLevelID, const float aScore = 0);
 		//CU::GrowingArray<Highscore> RetriveLocalHighcore(const int aLevelID, const float aScore = 0);
