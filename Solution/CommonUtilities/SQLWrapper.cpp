0f2ebfd4e142e50b14db36767d46530b4f3d92b8
LastVision <daniel.carlsson92@gmail.com>
2016-06-13 17:52:08 +0200

    Reduced timeout from 3 seconds to 1 second

--- Solution/CommonUtilities/SQLWrapper.cpp
+++ Solution/CommonUtilities/SQLWrapper.cpp
@@ -58,7 +58,7 @@ namespace CU
 		const char* aDatabaseName, unsigned long aClientFlag, const int aPort, const char* aUnixSocket)
 	{
 		myDatabaseName = aDatabaseName;
-		int timeout = 3;
+		int timeout = 1;
 		bool reconnect = true;
 		mysql_options(myConnection, MYSQL_OPT_READ_TIMEOUT, &timeout);
 		mysql_options(myConnection, MYSQL_OPT_WRITE_TIMEOUT, &timeout);
